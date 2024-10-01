90481d5e326269a5af46d642d5e0cfac005fd765
Abdullatif Khayat <abdullatif.khayat123@gmail.com>
2024-10-01 15:15:01 +0300

    Finished the landing page of the travel agency builder

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/magic (2).jpg',
+    title: 'Bali',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/magic (1).jpg',
+    title: 'Italia',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/ce744713e30e5f5d5f104eef3db18f94.jpg',
+    title: 'Portugal',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/135cc4d4b40e95512f8586d8afba42bf.jpg',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/a1e4118a2194424ba007d6cc421a81a2.jpg',
+    title: 'Istanbul, Turkey',
+  },
+];
+
+export const startingFeatures = [
+  'Find a destination that suits you and you want to visit',
+  'See what people think about it, watch images and 3D videos',
+  'Book a trip for your dream destination and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'New destinations',
+    subtitle:
+        'we have the latest updates with new world for you to try new destinations',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'New activities',
+    subtitle:
+        'In the latest update, we added new activities during our trips like: kayak, parachute...and much more.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/family.jpg',
+    title: 'Personnalized Journey for everyone',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/guide.jpg',
+    title: 'Expert Guidance during trips',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/group.jpg',
+    title: 'With one ticket you can explore many countries',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
